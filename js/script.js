22f9cc16e2d8f6cc769682b4fe5c30c8a56d06d0
Alone107 <123736524+Alone107@users.noreply.github.com>
2023-06-02 17:44:25 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -33,7 +33,8 @@ $('.slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+  autoplaySpeed: 12000,
+    infinite: true,
   });
 
 //   $('.video-slider').slick({
